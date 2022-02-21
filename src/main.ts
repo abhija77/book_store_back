20dd6a1f841818801d3b1080bdfe9c0cf8d0ad7e
abhija77 <ayoub.bhija77@gmail.com>
2022-02-21 12:24:08 +0100

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const server = await app.listen(3316);
+  app.enableCors();
   server.setTimeout(1800000); // 600,000=> 10Min, 1200,000=>20Min, 1800,000=>30Min
 
 }
